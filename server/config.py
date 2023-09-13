9c67dd7232aa0161b70a11bd280cca926f5c5b18
glendaandrews77 <138835509+glendaandrews77@users.noreply.github.com>
2023-09-12 20:06:53 -0700

    Add files via upload

---
+++ server/config.py
@@ -0,0 +1,12 @@
+developer = {
+    "first": "Gigi",
+    "last": "Bailey",
+    "age": "46",
+    "email": "tx2vabch@gmail.com",
+    "hobbies": ["code", "fishing"],
+    "address": {
+        "num": "12244",
+        "street": "homers",
+        "city": "temecula"
+    }
+}
